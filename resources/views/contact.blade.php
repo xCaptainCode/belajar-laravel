70783c004cde4ec26200341ed5e9473b206ae2e7
Captain Wannn <wannncaptain@gmail.com>
2025-09-10 16:55:43 +0700

    Sample form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,12 +3,6 @@
     <p class="text-2xl">Ini adalah halaman Contact</p>
 
     <form class="max-w-md mx-auto">
-      <div class="relative z-0 w-full mb-5 group" hidden>
-        <trix-editor type="text" name="floating_email" id="floating_email"
-          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder=" " required></trix-editor>
-      </div>
-
       <div class="relative z-0 w-full mb-5 group">
         <input type="email" name="floating_email" id="floating_email"
           class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -71,8 +65,8 @@
 
       <!-- Editor Quill -->
       <div class="mb-4">
-        <label class="block mb-1 font-medium">Konten</label>
-        <div id="quill-editor" class="h-64 bg-white border rounded-lg"></div>
+        <label class="block mb-1 font-medium">Isi Artikel</label>
+        <div id="quill-editor" class="h-64 bg-white border"></div>
         <input type="hidden" name="content" id="content">
       </div>
 
